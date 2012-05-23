2f5b01b0f09ae465c791bca661f6240de42603c9
Angelo Dell'Aera <buffer@olografix.org>
2012-05-23 11:30:01 +0200

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ sourcefiles  = ['src/pylibemu.c']
 setup(
     name         = "pylibemu",
     packages     = [],
-    version      = "0.2.0",
+    version      = "0.2.1",
     description  = "Libemu Python wrapper",
     url          = "",
     author       = "Angelo Dell'Aera",
