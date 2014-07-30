697ca9cb58c8c660778dbe722fd41d994cf45af3
Angelo Dell'Aera <buffer@olografix.org>
2014-07-30 11:52:26 +0200

    Updated copyright

--- src/pylibemu.pyx
+++ src/pylibemu.pyx
@@ -1,7 +1,7 @@
 #
 # pylibemu.pyx
 #
-# Copyright(c) 2011-2013 Angelo Dell'Aera <buffer@antifork.org>
+# Copyright(c) 2011-2014 Angelo Dell'Aera <buffer@antifork.org>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
